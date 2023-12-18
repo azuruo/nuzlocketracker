6f8b0f75428a794d9396fc123841901b7a006bcc
ATirado <azuru5039@gmail.com>
2023-12-18 18:16:08 -0500

    add register new user logic to controller

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -3,8 +3,36 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 exports.register = async (req, res) => {
-  // Register new user
-};
+    try {
+      // Check if the user already exists
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res.status(400).json({ message: "User already exists" });
+      }
+  
+      // Create a new user
+      user = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+      });
+  
+      // Hash the password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(user.password, salt);
+  
+      await user.save();
+  
+      // Create a JWT token
+      const payload = { userId: user._id };
+      const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
+  
+      res.status(201).json({ token });
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  };
 
 exports.login = async (req, res) => {
   // User login
