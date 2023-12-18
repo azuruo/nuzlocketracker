246e40bdc2e7a3ec9bddabe933527588ea740226
ATirado <azuru5039@gmail.com>
2023-12-18 18:27:02 -0500

    add team routes

---
+++ routes/teams.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const teamsController = require('../controllers/teamsController');
+const authMiddleware = require('../middleware/auth');
+
+router.get('/', authMiddleware, teamsController.getAllTeams);
+router.post('/', authMiddleware, teamsController.createTeam);
+router.get('/:teamId', authMiddleware, teamsController.getTeamById);
+router.put('/:teamId', authMiddleware, teamsController.updateTeam);
+router.delete('/:teamId', authMiddleware, teamsController.deleteTeam);
+
+module.exports = router;
