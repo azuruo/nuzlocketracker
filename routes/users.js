acf212171b97857fba941b3e56d0fe2f74daaed1
ATirado <azuru5039@gmail.com>
2023-12-18 18:10:18 -0500

    add user routes and controller

---
+++ routes/users.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const usersController = require('../controllers/usersController');
+const authMiddleware = require('../middleware/auth');
+
+router.post('/register', usersController.register);
+router.post('/login', usersController.login);
+router.get('/profile', authMiddleware, usersController.getProfile);
+router.put('/profile', authMiddleware, usersController.updateProfile);
+
+module.exports = router;
