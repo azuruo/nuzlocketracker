91e44b07e515ad730bb7a7501cad7c8891a3ef48
ATirado <azuru5039@gmail.com>
2023-12-18 18:40:01 -0500

    add get user profile controller logic

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -59,9 +59,19 @@ exports.register = async (req, res) => {
     }
   };
 
-exports.getProfile = async (req, res) => {
-  // Get user profile
-};
+  exports.getProfile = async (req, res) => {
+    try {
+      const user = await User.findById(req.userId).select('-password');
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+      res.json(user);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  };
+  
 
 exports.updateProfile = async (req, res) => {
   // Update user profile
