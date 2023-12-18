70c72073db9f02642c293145cfe546eaa1d223c2
ATirado <azuru5039@gmail.com>
2023-12-18 18:24:14 -0500

    add pokemonBoxController frame

---
+++ controllers/pokemonBoxController.js
@@ -0,0 +1,21 @@
+const PokemonBox = require('../models/PokemonBox');
+
+exports.getAllBoxes = async (req, res) => {
+  // Get all Pokémon boxes for a user
+};
+
+exports.createBox = async (req, res) => {
+  // Create a new Pokémon box
+};
+
+exports.getBoxById = async (req, res) => {
+  // Get a specific Pokémon box
+};
+
+exports.updateBox = async (req, res) => {
+  // Update a specific Pokémon box
+};
+
+exports.deleteBox = async (req, res) => {
+  // Delete a specific Pokémon box
+};
