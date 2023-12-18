0831ea1ce34d5fcacd3daafb261b26096bb5ce7d
ATirado <azuru5039@gmail.com>
2023-12-18 18:18:25 -0500

    add teamsController frame

---
+++ controllers/teamsController.js
@@ -0,0 +1,21 @@
+const Team = require('../models/Team');
+
+exports.getAllTeams = async (req, res) => {
+  // Get all teams for a user
+};
+
+exports.createTeam = async (req, res) => {
+  // Create a new team
+};
+
+exports.getTeamById = async (req, res) => {
+  // Get a specific team
+};
+
+exports.updateTeam = async (req, res) => {
+  // Update a specific team
+};
+
+exports.deleteTeam = async (req, res) => {
+  // Delete a specific team
+};
