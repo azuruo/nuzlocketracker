e5deea9e6d32d419403a3e01b80eebbdfd96e7fe
ATirado <azuru5039@gmail.com>
2023-12-18 18:45:15 -0500

    add updateProfile user controller logic

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -73,6 +73,22 @@ exports.register = async (req, res) => {
   };
   
 
-exports.updateProfile = async (req, res) => {
-  // Update user profile
-};
+  exports.updateProfile = async (req, res) => {
+    try {
+      const user = await User.findByIdAndUpdate(
+        req.userId,
+        { $set: req.body },
+        { new: true }
+      ).select('-password');
+  
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+  
+      res.json(user);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  };
+  
