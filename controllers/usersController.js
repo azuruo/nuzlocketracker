1e0e453019c89db8eedbc7d8617c7d675e8a269b
ATirado <azuru5039@gmail.com>
2023-12-18 18:12:41 -0500

    add user controller frame

---
+++ controllers/usersController.js
@@ -0,0 +1,19 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+exports.register = async (req, res) => {
+  // Register new user
+};
+
+exports.login = async (req, res) => {
+  // User login
+};
+
+exports.getProfile = async (req, res) => {
+  // Get user profile
+};
+
+exports.updateProfile = async (req, res) => {
+  // Update user profile
+};
