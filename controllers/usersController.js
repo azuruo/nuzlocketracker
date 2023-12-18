71c07cd2e72a599a6bf32aedb0f8a31ef7e3e161
ATirado <azuru5039@gmail.com>
2023-12-18 18:34:37 -0500

    add user login controller logic

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -34,9 +34,30 @@ exports.register = async (req, res) => {
     }
   };
 
-exports.login = async (req, res) => {
-  // User login
-};
+  exports.login = async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      let user = await User.findOne({ email });
+  
+      if (!user) {
+        return res.status(400).json({ message: "Invalid Credentials" });
+      }
+  
+      const isMatch = await bcrypt.compare(password, user.password);
+  
+      if (!isMatch) {
+        return res.status(400).json({ message: "Invalid Credentials" });
+      }
+  
+      const payload = { userId: user._id };
+      const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
+  
+      res.json({ token });
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  };
 
 exports.getProfile = async (req, res) => {
   // Get user profile
