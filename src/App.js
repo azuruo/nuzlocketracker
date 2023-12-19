3505441f6cc1793e3415f27ac0da256361a703bb
ATirado <azuru5039@gmail.com>
2023-12-19 01:36:00 -0500

    move components folder under src and update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login';
+import Register from './components/Register';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <nav>
+            {/* Navigation Links */}
+            <Link to="/login" className="App-link">Login</Link>
+            <Link to="/register" className="App-link">Register</Link>
+            {/* Add more navigation links if needed */}
+          </nav>
+          <Switch>
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+            {/* Define other routes here */}
+            {/* <Route exact path="/" component={Home} /> */}
+            {/* Add a default route for unknown paths */}
+            {/* <Route component={NotFound} /> */}
+          </Switch>
+        </header>
+      </div>
+    </Router>
   );
 }
 
