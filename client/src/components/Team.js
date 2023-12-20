7449f8cab20e8ef1bca256ab8822170f4a0d48ec
ATirado <azuru5039@gmail.com>
2023-12-20 01:45:35 -0500

    Refactor Team component to use Material-UI components

--- client/src/components/Team.js
+++ client/src/components/Team.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
 
 const Team = ({ data }) => {
   return (
-    <div>
-      <h2>My Team</h2>
-      {data ? (
-        <ul>
-          {data.map((member) => (
-            <li key={member.id}>{member.name}</li>
-          ))}
-        </ul>
-      ) : (
-        <p>No team data available.</p>
-      )}
-    </div>
+    <Card sx={{ margin: 2, backgroundColor: '#f0f0f0' }}>
+      <CardContent>
+        <Typography variant="h5" component="div">
+          My Team
+        </Typography>
+        {data ? (
+          <List>
+            {data.map((member) => (
+              <ListItem key={member.id}>{member.name}</ListItem>
+            ))}
+          </List>
+        ) : (
+          <Typography color="text.secondary">No team data available.</Typography>
+        )}
+      </CardContent>
+    </Card>
   );
 };
 
