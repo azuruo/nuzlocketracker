14da5e0cbebe309561248d7eb7f098633f2d259f
ATirado <azuru5039@gmail.com>
2023-12-19 21:50:21 -0500

    Update proxy and email field in Login component, add CORS package

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
-    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
     const navigate = useNavigate();
@@ -11,22 +11,22 @@ const Login = () => {
     const handleSubmit = async (event) => {
         event.preventDefault();
     
-        if (!username || !password) {
-            alert('Please enter both username and password');
+        if (!email || !password) {
+            alert('Please enter both email and password');
             return;
         }
         try {
-            const response = await axios.post('/api/users/login', { username, password });
+            const response = await axios.post('/api/users/login', { email, password });
             localStorage.setItem('token', response.data.token); 
             navigate('/');
         } catch (error) {
-            alert('Login failed: Incorrect credentials');
+            alert('Login failed: ' + (error.response?.data.message || 'Unknown error'));
         }
     };
 
     return (
         <form onSubmit={handleSubmit}>
-            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
+            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
             <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
             <button type="submit">Login</button>
         </form>
--- server.js
+++ server.js
@@ -1,23 +1,32 @@
 require('dotenv').config();
 const express = require('express');
-const app = express();
+const cors = require('cors'); // Import CORS package
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/users');
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-app.use(express.json());
-app.use('/api/users', userRoutes);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// User routes
+app.use('/api/users', userRoutes);
 
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log('MongoDB Connected'))
+  .then(() => {
+    console.log('MongoDB Connected');
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
   .catch((err) => console.error('Could not connect to MongoDB:', err));
