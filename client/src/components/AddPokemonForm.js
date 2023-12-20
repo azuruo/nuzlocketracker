0a6ab6474ef0fd465bedbbedd8d0a2ce586e7fc4
ATirado <azuru5039@gmail.com>
2023-12-20 04:05:13 -0500

    Add generation selection and Pokémon selection functionality

--- client/src/components/AddPokemonForm.js
+++ client/src/components/AddPokemonForm.js
@@ -1,43 +1,93 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import axios from 'axios';
+import MenuItem from '@mui/material/MenuItem';
 
 const AddPokemonForm = ({ onPokemonAdd }) => {
-  const [searchTerm, setSearchTerm] = useState('');
+  const [selectedGeneration, setSelectedGeneration] = useState('');
   const [loading, setLoading] = useState(false);
+  const [pokemonList, setPokemonList] = useState([]);
+  const [selectedPokemon, setSelectedPokemon] = useState(null);
 
-  const fetchPokemon = async () => {
-    setLoading(true);
-    try {
-      const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${searchTerm.toLowerCase()}`);
-      const pokemon = {
-        name: response.data.name,
-        sprite: response.data.sprites.front_default,
-      };
-      onPokemonAdd(pokemon);
-      setSearchTerm('');
-    } catch (error) {
-      console.error('Failed to fetch pokemon:', error);
-    } finally {
-      setLoading(false);
+  useEffect(() => {
+    const fetchPokemonByGeneration = async () => {
+      if (selectedGeneration) {
+        setLoading(true);
+        try {
+          const response = await axios.get(`https://pokeapi.co/api/v2/generation/${selectedGeneration.toLowerCase()}`);
+          const pokemonData = response.data.pokemon_species.map((pokemon) => {
+            return {
+              name: pokemon.name,
+            };
+          });
+          setPokemonList(pokemonData);
+        } catch (error) {
+          console.error('Failed to fetch Pokémon:', error);
+        } finally {
+          setLoading(false);
+        }
+      }
+    };
+
+    fetchPokemonByGeneration();
+  }, [selectedGeneration]);
+
+  const handlePokemonSelect = (event) => {
+    const selectedPokemonName = event.target.value;
+    const selectedPokemonData = pokemonList.find((pokemon) => pokemon.name === selectedPokemonName);
+    setSelectedPokemon(selectedPokemonData);
+  };
+
+  const handleAddPokemon = async () => {
+    if (selectedPokemon) {
+      try {  
+        // Add to Box
+        const responseBox = await axios.post('/api/addPokemonToBox', { pokemon: selectedPokemon });
+        console.log('Added to Box:', responseBox.data);
+        // Add to Team
+        const responseTeam = await axios.post('/api/addPokemonToTeam', { pokemon: selectedPokemon });
+        console.log('Added to Team:', responseTeam.data);
+      } catch (error) {
+        console.error('Failed to add Pokémon:', error);
+      }
     }
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    fetchPokemon();
-  };
-
   return (
-    <form onSubmit={handleSubmit}>
+    <form>
       <TextField
-        label="Search Pokémon"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
+        label="Select Pokémon Generation"
+        select
+        variant="outlined"
+        fullWidth
+        value={selectedGeneration}
+        onChange={(e) => setSelectedGeneration(e.target.value)}
         disabled={loading}
-      />
-      <Button type="submit" disabled={loading}>Add</Button>
+      >
+        <MenuItem value="1">Generation I</MenuItem>
+        <MenuItem value="2">Generation II</MenuItem>
+        <MenuItem value="3">Generation III</MenuItem>
+        {/* Add more generations as needed */}
+      </TextField>
+      <TextField
+        label="Select Pokémon"
+        select
+        variant="outlined"
+        fullWidth
+        value={selectedPokemon ? selectedPokemon.name : ''}
+        onChange={handlePokemonSelect}
+        disabled={loading}
+      >
+        {pokemonList.map((pokemon) => (
+          <MenuItem key={pokemon.name} value={pokemon.name}>
+            {pokemon.name}
+          </MenuItem>
+        ))}
+      </TextField>
+      <Button variant="contained" color="primary" onClick={handleAddPokemon} disabled={!selectedPokemon}>
+        Add to Box
+      </Button>
     </form>
   );
 };
