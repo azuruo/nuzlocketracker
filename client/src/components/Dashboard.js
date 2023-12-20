7ac2b9d17851bc1d394c838e2206947e8643c396
ATirado <azuru5039@gmail.com>
2023-12-20 03:20:00 -0500

    Refactor Dashboard component and add drag and drop functionality

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,14 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Team from './Team';
 import Box from './Box';
+import AddPokemonForm from './AddPokemonForm';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
+import { DragDropContext } from 'react-beautiful-dnd';
+import CircularProgress from '@mui/material/CircularProgress';
+
 
 const Dashboard = () => {
-  const [team, setTeam] = useState(null);
-  const [box, setBox] = useState(null);
+  const [team, setTeam] = useState([]);
+  const [box, setBox] = useState([]);
   const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetchTeamAndBox = async () => {
@@ -20,37 +25,96 @@ const Dashboard = () => {
           },
         };
 
-        const teamResponse = await axios.get('/api/teams/myteam', config);
-        const boxResponse = await axios.get('/api/pokemonBoxes/mybox', config);
+        const teamResponse = await axios.get('/api/teams', config);
+        const boxResponse = await axios.get('/api/pokemonBoxes', config);
 
         setTeam(teamResponse.data);
         setBox(boxResponse.data);
       } catch (err) {
-        setError(err.response.data.message || 'An error occurred');
+        setError(err.response?.data.message || 'An error occurred');
       }
     };
 
-    fetchTeamAndBox();
+    setIsLoading(true);
+    fetchTeamAndBox().finally(() => setIsLoading(false));
   }, []);
 
-  if (error) {
+  const onDragEnd = (result) => {
+    const { destination, source } = result;
+  
+    // If there's no destination (dropped outside the lists), do nothing
+    if (!destination) {
+      return;
+    }
+  
+    // Check if the location of the draggable item changed
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+  
+    let newTeam = [...team];
+    let newBox = [...box];
+  
+    // Moving within the same list
+    if (source.droppableId === destination.droppableId) {
+      const sourceList = source.droppableId === 'team' ? newTeam : newBox;
+      const [removed] = sourceList.splice(source.index, 1);
+      sourceList.splice(destination.index, 0, removed);
+    } else {
+      // Moving between lists
+      const sourceList = source.droppableId === 'team' ? newTeam : newBox;
+      const destList = destination.droppableId === 'team' ? newTeam : newBox;
+      const [removed] = sourceList.splice(source.index, 1);
+  
+      // Add to destination list
+      destList.splice(destination.index, 0, removed);
+    }
+  
+    // Update state
+    setTeam(newTeam);
+    setBox(newBox);
+  };
+  
+  const handleAddPokemonToBox = (newPokemon) => {
+    if (box.length >= 30) {
+      alert('Box is full!'); // Notify user if the box is full
+      return;
+    }
+    setBox(currentBox => [newPokemon, ...currentBox]);
+  };
+  
+  if (isLoading) {
     return (
-      <div>
-        <h1>Dashboard</h1>
-        <p>{error}</p>
+      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '50px' }}>
+        <CircularProgress />
       </div>
     );
   }
-
-return (
-  <Container maxWidth="sm" sx={{ marginTop: 4 }}>
-    <Typography variant="h4" component="h1" gutterBottom>
-      Dashboard
-    </Typography>
-    <Team data={team} /> {/* Pass the team data as props to the Team component */}
-      <Box data={box} /> {/* Pass the box data as props to the Box component */}
-  </Container>
-);
+  if (error) {
+    return (
+      <div style={{ textAlign: 'center', marginTop: '50px' }}>
+        <Typography variant="h6" color="error">
+          {error}
+        </Typography>
+      </div>
+    );
+  }
+  
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Container maxWidth="sm" sx={{ marginTop: 4 }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Dashboard
+        </Typography>
+        <AddPokemonForm onPokemonAdd={handleAddPokemonToBox} />
+        <Team data={team} />
+        <Box data={box} isDropDisabled={team?.length >= 6} />
+      </Container>
+    </DragDropContext>
+  );
 };
 
 export default Dashboard;
