0ce4b03337fb422cc9f158a362b6398952d3f812
ATirado <azuru5039@gmail.com>
2023-12-19 19:05:27 -0500

    Update routes and server configuration

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const usersController = require('../controllers/usersController');
 const authMiddleware = require('../middleware/auth');
 
-router.post('/register', usersController.register);
+router.post('/api/users/register', usersController.register);
 router.post('/login', usersController.login);
 router.get('/profile', authMiddleware, usersController.getProfile);
 router.put('/profile', authMiddleware, usersController.updateProfile);
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-require('dotenv').config();
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
@@ -15,7 +15,6 @@ app.listen(PORT, () => {
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true
 })
 .then(() => console.log('MongoDB Connected'))
 .catch(err => console.error('Could not connect to MongoDB:', err));
