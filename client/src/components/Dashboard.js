502a83ce9d0405db8af5c23c1c4e299ab7509d79
ATirado <azuru5039@gmail.com>
2023-12-20 22:28:29 -0500

    Refactor Dashboard component and add logout functionality

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,19 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import CircularProgress from '@mui/material/CircularProgress';
-import PokemonBox from './PokemonBox'; // Import the PokemonBox component
+import Button from '@mui/material/Button';
+import PokemonBox from './PokeCard';
 import AddPokemonForm from './AddPokemonForm';
 
-const Dashboard = () => {
+const Dashboard = ({ setLoggedIn }) => { // Accept setLoggedIn as a prop
   const [box, setBox] = useState([]);
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null); // Added error state
+  const [isLoading, setIsLoading] = useState(true);
+  const navigate = useNavigate(); // Added navigate hook
 
   useEffect(() => {
     const fetchBox = async () => {
-      setIsLoading(true);
       try {
         const token = localStorage.getItem('token');
         const config = {
@@ -22,10 +24,11 @@ const Dashboard = () => {
           },
         };
 
-        const boxResponse = await axios.get('/api/pokemonBoxes', config);
-        setBox(boxResponse.data);
-      } catch (err) {
-        setError(err.response?.data.message || 'An error occurred');
+        const response = await axios.get('/api/pokemonBoxes/userBox', config);
+        setBox(response.data);
+      } catch (error) {
+        console.error('Error fetching box:', error);
+        setError(error.response?.data.message || 'An error occurred'); // Set error message
       } finally {
         setIsLoading(false);
       }
@@ -40,6 +43,12 @@ const Dashboard = () => {
       return;
     }
     setBox(currentBox => [newPokemon, ...currentBox]);
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    setLoggedIn(false); // Update the loggedIn state
+    navigate('/'); // Navigate to home page
   };
 
   // Render loading state
@@ -65,6 +74,9 @@ const Dashboard = () => {
   // Render dashboard with Pokemon box
   return (
     <Container maxWidth="sm" sx={{ marginTop: 4 }}>
+      <Button variant="contained" color="secondary" onClick={handleLogout} sx={{ mb: 2 }}>
+        Log Out
+      </Button>
       <Typography variant="h4" component="h1" gutterBottom>
         Dashboard
       </Typography>
