7f6e8f500080553d8e4c9815e02f538ce9f56c96
ATirado <azuru5039@gmail.com>
2023-12-21 13:41:32 -0500

    Add MongoDB connection and update server start code

--- server.js
+++ server.js
@@ -24,7 +24,18 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/build/index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('MongoDB Connected');
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.error('Could not connect to MongoDB:', err));
+
+  
